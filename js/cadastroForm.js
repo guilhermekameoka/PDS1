8cc3d0e3b6f6ca03dda8220a84e7b29617df26a0
Guilherme Kameoka <guilherme.kameoka@outlook.com>
2025-04-02 12:40:23 -0300

    feat: Enhance user registration form with improved error handling and page load event

--- js/cadastroForm.js
+++ js/cadastroForm.js
@@ -1,38 +1,44 @@
-document
-  .getElementById("cadastroForm")
-  .addEventListener("submit", async function (event) {
-    event.preventDefault();
+document.addEventListener("DOMContentLoaded", function () {
+  document
+    .getElementById("cadastroForm")
+    .addEventListener("submit", async function (event) {
+      event.preventDefault();
 
-    const usuario = {
-      nome: document.getElementById("nome").value,
-      idade: document.getElementById("idade").value,
-      email: document.getElementById("email").value,
-      telefone: document.getElementById("telefone").value,
-      cep: document.getElementById("cep").value,
-      rua: document.getElementById("rua").value,
-      numero: document.getElementById("numero").value,
-      cidade: document.getElementById("cidade").value,
-      senha: document.getElementById("senha").value,
-    };
+      const usuario = {
+        nome: document.getElementById("nome").value,
+        idade: document.getElementById("idade").value,
+        email: document.getElementById("email").value,
+        telefone: document.getElementById("telefone").value,
+        cep: document.getElementById("cep").value,
+        rua: document.getElementById("rua").value,
+        numero: document.getElementById("numero").value,
+        cidade: document.getElementById("cidade").value,
+        senha: document.getElementById("senha").value,
+      };
 
-    try {
-      const response = await fetch("http://localhost:3000/cadastro", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(usuario),
-      });
+      try {
+        const response = await fetch("http://localhost:3000/cadastro", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(usuario),
+        });
 
-      const data = await response.json();
-      if (response.ok) {
-        alert("Cadastro realizado com sucesso!");
-        window.location.href = "./login.html";
-      } else {
-        alert("Erro: " + data.error);
+        const data = await response.json();
+        if (response.ok) {
+          alert("Cadastro realizado com sucesso!");
+          window.location.href = "../home.html";
+        } else if (response.status === 400) {
+          alert(
+            "Erro: Dados inválidos. Por favor, verifique as informações e tente novamente."
+          );
+        } else {
+          alert("Erro: " + data.error);
+        }
+      } catch (error) {
+        console.error("Erro ao enviar dados:", error);
+        alert("Erro ao cadastrar.");
       }
-    } catch (error) {
-      console.error("Erro ao enviar dados:", error);
-      alert("Erro ao cadastrar.");
-    }
-  });
+    });
+});
