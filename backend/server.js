180a2999d00a58bbb81a4997e9129451e47aff6f
Guilherme Kameoka <guilherme.kameoka@outlook.com>
2025-04-02 16:47:31 -0300

    feat: Enhance server routing and static file serving in server.js

--- backend/server.js
+++ backend/server.js
@@ -2,17 +2,33 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const path = require("path");
 
 // npm install express dotenv cors body-parser
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, "../frontend"), { extensions: ['html', 'htm'] }));
+app.use("/assets", express.static(path.join(__dirname, "../assets")));
 
 const cadastroRoutes = require("./routes/cadastro");
 app.use("/cadastro", cadastroRoutes);
+app.use("/js", express.static(path.join(__dirname, "../js")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/comum/index.html"));
+});
+
+app.get("/:page", (req, res) => {
+  let page = req.params.page;
+  if (!page.endsWith(".html")) {
+    page += ".html";
+  }
+  res.sendFile(path.join(__dirname, `../frontend/comum/${page}`));
+});
 
 // Inicia o servidor na porta especificada
 app.listen(port, () => {
