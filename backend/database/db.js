08c53f028f03ccf68ffaf61773813d0c1c8ba65a
Guilherme Kameoka <guilherme.kameoka@outlook.com>
2025-04-03 12:23:50 -0300

    refactor: Switch to MySQL connection pool for improved database handling

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,21 +1,14 @@
-const mysql = require("mysql2");
-require("dotenv").config();
-// npm install mysql2 dotenv bcrypt
+const dotenv = require("dotenv").config();
+const mysql = require("mysql2/promise");
 
-const db = mysql.createConnection({
+const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
-db.connect((err) => {
-  if (err) {
-    console.error("Erro ao conectar ao MySQL:", err);
-    process.exit(1); // Finaliza o processo em caso de erro na conexão
-  } else {
-    console.log("Conectado ao MySQL");
-  }
-});
-
-module.exports = db;
+module.exports = pool;
