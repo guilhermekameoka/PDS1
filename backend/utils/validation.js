9d3b3e9888ac52fa46fd4be1dfb142abe1f0d322
Guilherme Kameoka <guilherme.kameoka@outlook.com>
2025-04-11 18:25:57 -0300

    feat: Update validation schemas to include tipo_usuario and adjust data types for medicamento fields

--- backend/utils/validation.js
+++ backend/utils/validation.js
@@ -10,15 +10,18 @@ const userSchema = Joi.object({
   numero: Joi.string().required(),
   cidade: Joi.string().required(),
   senha: Joi.string().min(6).required(),
+  tipo_usuario: Joi.string().valid('medico', 'idoso', 'cuidador')
 });
 
 const medicamentoSchema = Joi.object({
   nome: Joi.string().required(),
   data_inicial: Joi.date().required(),
   data_final: Joi.date().required(),
-  frequencia: Joi.number().positive().required(),
+  frequencia: Joi.string().required(),
   hora: Joi.string().required(),
   dose: Joi.string().required(),
+  id_usuario: Joi.number().positive().required(),
+  id_medico: Joi.number().positive().required()
 });
 
 module.exports = {
