2d1632c87af34f3180061e0134b5705550b28fe4
Guilherme Kameoka <guilherme.kameoka@outlook.com>
2025-04-11 18:25:42 -0300

    feat: Add usuarios route to handle requests for elderly users

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,11 +2,13 @@ const express = require("express");
 const cadastroRoutes = require("./cadastro");
 const loginRoutes = require("./login");
 const regMedicamentoRoutes = require("./regMedicamento");
+const usuariosRoutes = require("./usuarios");
 
 const router = express.Router();
 
 router.use("/cadastro", cadastroRoutes);
 router.use("/login", loginRoutes);
 router.use("/medicamento", regMedicamentoRoutes);
+router.use("/usuarios", usuariosRoutes);
 
 module.exports = router;
